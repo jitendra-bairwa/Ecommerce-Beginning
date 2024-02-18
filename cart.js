dbd62c3d8b6ca939367e498f41f7eeacf5c697a8
jitendra_bairwa <jitendrabairwa225@gmail.com>
2024-02-18 15:38:47 +0530

    card remove functionlity

--- cart.js
+++ cart.js
@@ -5,5 +5,11 @@ const cartContainer = document.getElementById('cart');
 
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
+cartContainer.addEventListener('click', (event) => {
+    cart = cart.filter(({ _id }) => _id !== event.target.dataset.id);
+    cartContainer.innerHTML = "";
+    createProductCard(cart, cartContainer, findProductInCart, 'cart');
+    localStorage.setItem('cart', JSON.stringify(cart));
+})
 
-createProductCard(cart,cartContainer,findProductInCart,'cart');
+createProductCard(cart, cartContainer, findProductInCart, 'cart');
