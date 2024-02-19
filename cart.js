474edd7f99cdad990a91619ff6d42833af31caeb
jitendra_bairwa <jitendrabairwa225@gmail.com>
2024-02-19 11:02:22 +0530

    price cart is completed

--- cart.js
+++ cart.js
@@ -1,4 +1,3 @@
-// import { createProductCard } from "./createProductCard.js";
 import { findProductInCart } from "./utils/findProductInCart.js";
 import { createHorizontalProductCard } from "./createHorizontalProductCard.js";
 
@@ -13,5 +12,22 @@ cartContainer.addEventListener('click', (event) => {
     localStorage.setItem('cart', JSON.stringify(cart));
 })
 
-// createProductCard(cart, cartContainer, findProductInCart, 'cart');
+const cartLength = document.querySelector('.item-count');
+cartLength.innerText = JSON.parse(localStorage.getItem('cart')).length;
+
+const productPrice = document.querySelector('.product-price');
+const priceAfterDiscount = JSON.parse(localStorage.getItem('cart')).reduce((acc, curr) => acc + curr.newPrice, 0);
+productPrice.innerText = priceAfterDiscount;
+
+const discount = document.querySelectorAll(".discounted-amount");
+const priceBeforeDiscount = JSON.parse(localStorage.getItem('cart')).reduce((acc, curr) => acc + curr.oldPrice, 0);
+const discountedAmount = priceBeforeDiscount - priceAfterDiscount;
+for (let element of discount) {
+    element.innerText = discountedAmount;
+}
+
+const totalAmount = document.querySelector(".total-amount");
+totalAmount.innerText = priceAfterDiscount + 100;
+
+
 createHorizontalProductCard(cart, cartContainer, findProductInCart, 'cart');
